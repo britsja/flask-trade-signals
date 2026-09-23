bc3d785f23028ac9f3ed32ddc07a210981cf9477
Jan Brits <pcerservices@gmail.com>
2026-09-23 15:43:34 -0400

    refactored code

--- app/routes/trading.py
+++ app/routes/trading.py
@@ -9,19 +9,28 @@ class TradingWebHook(BaseModel):
     action: Literal["BUY", "SELL"]
     price: float = Field(gt=0)
 
-@trading_bp.route("/webhook", methods=["POST"])
-def receive_trade_request():  
+def execute_trade(validated_trade: TradingWebHook) -> str:
+    return f"{validated_trade.action} executed for {validated_trade.ticker} at {validated_trade.price}"
 
+@trading_bp.route("/webhook", methods=["POST"])
+def receive_trade_request():
   data = request.get_json()
+
+  if not data:
+     return jsonify({"error", "No JSON payload received"}), 400
 
   try:
     validated_trade = TradingWebHook(**data)
   except ValidationError as e:
     return jsonify({"error": e.errors()}), 400  
 
-  print(f"Executing trade")
-  def execute_trade(validated_trade: TradingWebHook):
-    return f"{validated_trade.action} executed for {validated_trade.ticker} at {validated_trade.price}"
+  print(f"Signal validated for {validated_trade.ticker}. Dispatching execution...")
 
-  return execute_trade(validated_trade), 202
+  execution_result = execute_trade(validated_trade)
+
+  return jsonify({
+     "status": "completed successfully",
+     "message": execution_result
+  }), 202
+
 

19c4a94f6c2acd8808d7bac4178c1783b99ca3c2
Jan Brits <pcerservices@gmail.com>
2026-09-23 15:34:35 -0400

    added trade api request

--- app/routes/trading.py
+++ app/routes/trading.py
@@ -1,17 +1,27 @@
 from flask import Blueprint, request, jsonify
+from pydantic import BaseModel, Field, ValidationError
+from typing import Literal
 
 trading_bp = Blueprint("trading", __name__)
 
+class TradingWebHook(BaseModel):
+    ticker: str = Field(pattern=r"^[A-Z]+$")
+    action: Literal["BUY", "SELL"]
+    price: float = Field(gt=0)
+
 @trading_bp.route("/webhook", methods=["POST"])
-def receive_trade_request():
+def receive_trade_request():  
+
   data = request.get_json()
-  if not data or "target_url" not in data:
-    return jsonify({"error": "Missing target_url parameter"}), 400
+
+  try:
+    validated_trade = TradingWebHook(**data)
+  except ValidationError as e:
+    return jsonify({"error": e.errors()}), 400  
 
   print(f"Executing trade")
+  def execute_trade(validated_trade: TradingWebHook):
+    return f"{validated_trade.action} executed for {validated_trade.ticker} at {validated_trade.price}"
 
-  return jsonify({
-    "status": "executed",
-    "message": "completed"
-  }), 202
+  return execute_trade(validated_trade), 202
 
